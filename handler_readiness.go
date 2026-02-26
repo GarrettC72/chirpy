350d4c7b2abce3f0def6816c3109e3b0507993d7
GarrettC72 <garrettchen2@gmail.com>
2026-02-26 17:46:13 -0500

    Added logo image and readiness handler

---
+++ handler_readiness.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handlerReadiness(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
+}
