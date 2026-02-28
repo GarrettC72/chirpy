95f3b6a9a68de50a46ca7d90870df5690e4152e8
GarrettC72 <garrettchen2@gmail.com>
2026-02-27 20:18:37 -0500

    Added handlers for metrics and specified methods for routes

--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiConfig := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	fsHandler := apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", fsHandler))
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiConfig.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiConfig.handlerReset)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
---
+++ reset.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerReset(writer http.ResponseWriter, request *http.Request) {
+	cfg.fileserverHits.Store(0)
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("Hits reset to 0"))
+}
